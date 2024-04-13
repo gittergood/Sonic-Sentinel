a77aea92efec9e9d5979d8c6c75e4127b08dc409
edelmj <108199579+edelmj@users.noreply.github.com>
2024-04-12 23:26:36 -0400

    Add files via upload
    Interfacing with microphone

---
+++ I2S_Microphone/I2S_Microphone.ino
@@ -0,0 +1,48 @@
+#include <driver/i2s.h>
+
+#define I2S_SCK  14
+#define I2S_WS   15
+#define I2S_SD   32
+
+const i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = 44100,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
+    .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB,
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count = 8,
+    .dma_buf_len = 64,
+    .use_apll = false,
+    .tx_desc_auto_clear = false,
+    .fixed_mclk = 0
+};
+
+const i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = I2S_PIN_NO_CHANGE,
+    .data_in_num = I2S_SD
+};
+
+void setup() {
+
+  Serial.begin(115200);
+
+  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_NUM_0, &pin_config);
+
+  Serial.println("I2S microphone initialized");
+}
+
+void loop() {
+
+  int32_t buffer[64];
+  
+  size_t bytes_read;
+  i2s_read(I2S_NUM_0, &buffer, sizeof(buffer), &bytes_read, portMAX_DELAY);
+
+  if (bytes_read > 0) {
+    Serial.println(buffer[0]);
+  }
+}
